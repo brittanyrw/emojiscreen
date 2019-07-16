2cad8113f8a02ba0bfb4f81a15d75d1a4efc71d2
Brittany Walker <breneewalker@gmail.com>
2019-07-16 00:00:29 -0400

    remove multi-type javascript and css

--- app.js
+++ app.js
@@ -15,15 +15,6 @@ $(document).ready(function() {
           for (var j = 0; j < data[i].emojiImgs.length; j++){
             imgs += '<img src="../emojis/' + data[i].emojiImgs[j] + '" alt="' + data[i].emojiImgs[j] + '">';
           }
-          var type = '';
-          for (var g = 0; g < data[i].type.length; g++){ 
-            if(data[i].type.length > 1){
-              type += '<span class="type multi-type">' + data[i].type[g] + '</span>';
-            } else {
-              type = '<span class="type">' + data[i].type[g] + '</span>';
-            }
-            
-          }
           
           emojiCard += "<div class='emoji-card'><div class='hint-container'><i class='fas fa-question-circle'></i><p class='hint'><span class='type'>" + data[i].type + "</span></p></div><div class='emoji-images'>" + imgs + "</div><div class='emoji-card-title hide-card'><h2>" + data[i].title + " (" + data[i].year + ")" + "</h2></div></div>";
         }
