a5bb0369aa9543bd3cba1dcf4463dc3d89130a33
Li Ke <sr.frontenddev210@gmail.com>
2019-07-21 14:03:33 +0800

    Fix Issue#42 - Reorganize the app file structure so that all shows/movies are not in one JSON file

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 $(document).ready(function() {
 
+    var emojiCard = '';
     var displayResources = $("#emojis");
 
+    // Musicals
     $.ajax({
       type: "GET",
-      url: "../data.json", 
+      url: "../data/musicals/data.json", 
       success: function(data) {
-
-        var emojiCard = '';
         
         for (var i in data) {
-          
+          emojiCard += "<div class='emoji-card'><div class='hint-container'><i class='fas fa-question-circle'></i><p class='hint'><span class='type'>" + data[i].type + "</span></p></div><div class='emoji-images'>" + data[i].emojiImgs + "</div><div class='emoji-card-title hide-card'><h3>" + data[i].title + " (" + data[i].year + ")" + "</h3></div></div>";
+        }
+        
+        displayResources.html(emojiCard);
+        twemoji.parse(document.body);
+      }
+    });
+
+    // TV shows
+    $.ajax({
+      type: "GET",
+      url: "../data/tv/data.json", 
+      success: function(data) {
+
+        for (var i in data) {
+          emojiCard += "<div class='emoji-card'><div class='hint-container'><i class='fas fa-question-circle'></i><p class='hint'><span class='type'>" + data[i].type + "</span></p></div><div class='emoji-images'>" + data[i].emojiImgs + "</div><div class='emoji-card-title hide-card'><h3>" + data[i].title + " (" + data[i].year + ")" + "</h3></div></div>";
+        }
+        
+        displayResources.html(emojiCard);
+        twemoji.parse(document.body);
+      }
+    });
+
+    // Movies
+    $.ajax({
+      type: "GET",
+      url: "../data/movies/data.json", 
+      success: function(data) {
+
+        for (var i in data) {
           emojiCard += "<div class='emoji-card'><div class='hint-container'><i class='fas fa-question-circle'></i><p class='hint'><span class='type'>" + data[i].type + "</span></p></div><div class='emoji-images'>" + data[i].emojiImgs + "</div><div class='emoji-card-title hide-card'><h3>" + data[i].title + " (" + data[i].year + ")" + "</h3></div></div>";
         }
         
