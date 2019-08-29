40bb2eade0eb27a442bdc7b07adc9bfddc4c5d6b
Seniru Pasan Indira <senirupasan@gmail.com>
2019-08-29 10:21:37 +0530

    Converted jQuery to vanilla JS

--- app.js
+++ app.js
@@ -1,34 +1,4 @@
-$(document).ready(function() {
-  // Create a variable for the container to hold the emoji cards.
-  var emojiCardContainer = $("#emojis");
-
-  // Create a variable for the emoji cards.
-  var emojiCard = "";
-
-  // Run the random order function below on the data inside data.js. This will display the cards in a random order on the page every time the page is refreshed.
-  shuffle(emojiItems);
-
-  // Loop through the data from the data.js file and insert parts of the data into HTML. On each loop, we are appending a new card with the HTML below.
-  for (var i in emojiItems) {
-    emojiCard +=
-      "<div class='emoji-card' data-filter='" + emojiItems[i].type +
-    "'><div class='hint-container'><i class='fas fa-question-circle'></i><p class='hint'><span class='type'>" + emojiItems[i].type +
-    "</span></p></div><div class='emoji-images'>" + emojiItems[i].emojiImgs +
-    "</div><div class='emoji-card-title hide-card'><h3>" + emojiItems[i].title +
-    " (" + emojiItems[i].year + ")" + "</h3></div></div>";
-  }
-
-  // Append the emoji card variable, which has all of the emoji cards to the initial variable we created that was for the container to hold the cards.
-  emojiCardContainer.html(emojiCard);
-
-  // Run Twemoji to change all emojis to Twitter emojis.
-  twemoji.parse(document.body);
-
-  // Add the count of number of shows/movies to the footer.
-  $("footer span").append(emojiItems.length);
-    
-
-  // Display movies and show in a random order, the random order will refresh on page reload. This function is used above before the cards are rendered on the page.
+// Display movies and show in a random order, the random order will refresh on page reload. This function is used above before the cards are rendered on the page.
   function shuffle(array) {
     var currentIndex = array.length,
       temporaryValue,
@@ -45,67 +15,90 @@ $(document).ready(function() {
     return array;
   }
 
-  // The code that runs the filter buttons at the top of the page. This currently allows users to filter by 'type' (ie musical, movie or tv show).
-  $("#filters button").each(function() { 
-    $(this).on("click", function() {
-      var filtertag = $(this).attr("data-filter");
-      $("#message").hide();
-      $("div.emoji-card-title").addClass("hide-card");
-      if (filtertag == "view-all") { // If the user clicks on view all, show all cards.
-        $("div.emoji-card").show();
-      } else if ( // If the user clicks on movies, musicals or tv shows, show the cards that fall into that category and hide all cards that do not fall into that category.
-        $("div.emoji-card[data-filter='" + filtertag + "']").length > 0
-      ) {
-        $("div.emoji-card").show();
-        $(
-          "div.emoji-card:not([data-filter='" + filtertag + "'])"
-        ).hide();
-      } else { // If there are no cards that match the filter, display a message that says that there are no cards for that category.
-        $("div.emoji-card").hide();
-        $("#message").show();
-        $("#message").html(
-          "<p>There are no " + filtertag + " cards on this page. 🙁</p>"
-        );
-      }
+// Handles the onload event (ready() in jquery)
+window.onload = () => {
+    //Create a variable for the container to hold the emoji cards
+    var emojiCardContainer = document.getElementById("emojis");
+    //Create a variable for emoji cards
+    var emojiCard = "";
+    // Run the random order function below on the data inside data.js. This will display the cards in a random order on the page every time the page is refreshed.
+    shuffle(emojiItems);
+     // Loop through the data from the data.js file and insert parts of the data into HTML. On each loop, we are appending a new card with the HTML below.
+    for (let e of emojiItems) {
+        emojiCard += "<div class='emoji-card' data-filter='" + e.type +
+    "'><div class='hint-container'><i class='fas fa-question-circle'></i><p class='hint'><span class='type'>" + e.type +
+    "</span></p></div><div class='emoji-images'>" + e.emojiImgs +
+    "</div><div class='emoji-card-title hide-card'><h3>" + e.title +
+    " (" + e.year + ")" + "</h3></div></div>";
+    }
+    // Append the emoji card variable, which has all of the emoji cards to the initial variable we created that was for the container to hold the cards.
+    emojiCardContainer.innerHTML = emojiCard;
+    // Run Twemoji to change all emojis to Twitter emojis.
+    twemoji.parse(document.body);
+    // Add the count of number of shows/movies to the footer.
+    document.querySelector("footer span").innerHTML += emojiItems.length;
+    
+    // The code that runs the filter buttons at the top of the page. This currently allows users to filter by 'type' (ie musical, movie or tv show).
+    Object.values(document.querySelectorAll("#filters button")).forEach(e => {
+        e.onclick = () => {
+            let filterTag = e.getAttribute("data-filter");
+            console.log(filterTag)
+            document.getElementById("message").style.display = "none";
+            Object.values(document.querySelectorAll("div.emoji-card-title")).forEach(filt => {
+                filt.classList.add("hide-card");
+            });
+            // If the user clicks on view all, show all cards
+            if (filterTag === "view-all") {
+                console.log("Showing all!");
+                Object.values(document.querySelectorAll("div.emoji-card")).forEach(s => {
+                    s.style.display = "block";
+                })
+            } else if (document.querySelectorAll(`div.emoji-card[data-filter='${filterTag}']`).length > 0) { // If the user clicks on movies, musicals or tv shows, show the card that fall into that category and hide all cards that do not fall into that category.
+                Object.values(document.querySelectorAll("div.emoji-card")).forEach(g => {
+                    g.style.display = g.getAttribute("data-filter") === filterTag ? "block" : "none";
+                })
+            } else {
+                Object.values(document.querySelectorAll("div.emoji-card")).forEach(n => {
+                    n.style.display = "none";
+                });
+                let msg = document.getElementById("message");
+                msg.style.display = "block";
+                msg.innerHTML = `<p>There are no ${filterTag} cards on this page. 🙁</p>"`;
+                
+            }
+        }
     });
-  });
-
-  // Display total count of movies and shows in the footer
-  $("#emojis").on("click", ".emoji-images", function() {
-    $(this)
-      .siblings(".emoji-card-title")
-      .toggleClass("hide-card");
-  });
-
-  // Display a hint (type ie tv, movie or musical) when hovering over the question mark.
-  $("#emojis").on("mouseover", ".hint-container", function() {
-    $(this)
-      .find(".hint")
-      .addClass("hint-reveal");
-  });
-
-  // Hide hint (type ie tv, movie or musical) when the user stops hovering over the question mark.
-  $("#emojis").on("mouseleave", ".hint-container", function() {
-    $(this)
-      .find(".hint")
-      .removeClass("hint-reveal");
-  });
-
-  // Toggle to expand or hide all of the movie/show names by clicking an icon
-  $(".btn-reveal-all").click(function() {
-    var title = $(this).attr("title");
-    title =
-      title.search(/reveal/i) === -1
-        ? title.replace(/hide/i, "Reveal")
-        : title.replace(/reveal/i, "Hide");
-    $(this).attr("title", title);
-
-    $(this)
-      .find("i")
-      .toggleClass(["fa-eye", "fa-eye-slash"]);
-    $("#emojis")
-      .find(".emoji-card-title")
-      .toggleClass("hide-card");
-  });
-});
-
+    
+    Object.values(document.querySelectorAll("div.emoji-card")).forEach(card => {
+        //   Displaying / Hiding the title when clicking the emoji card
+        card.onclick = () => {
+            card.getElementsByClassName("emoji-card-title")[0].classList.toggle("hide-card");
+        }
+        // Displaying a hint(type ie tv, movie or musical) when hovering over the question mark
+        card.getElementsByClassName("hint-container")[0].onmouseover = () => {
+            card.getElementsByClassName("hint")[0].classList.add("hint-reveal");
+        }
+        card.getElementsByClassName("hint-container")[0].onmouseleave = () => {
+            card.getElementsByClassName("hint")[0].classList.remove("hint-reveal")
+        }
+    })
+    let eye = document.getElementsByClassName("btn-reveal-all")[0]
+    eye.onclick = () => {
+        // Changes the title of 'eye' icon to reveal/hide and icon according to the situation
+        //eye.setAttribute("title", eye.getAttribute("title") === "Hide All" ? "Reveal All" : "Hide All");
+        let icon = eye.getElementsByTagName("i")[0]
+        if (eye.getAttribute("title") === "Reveal All") {
+            icon.classList.remove("fa-eye-slash");
+            icon.classList.add("fa-eye");
+            eye.setAttribute("title", "Hide All");
+        } else {
+            icon.classList.remove("fa-eye");
+            icon.classList.add("fa-eye-slash");
+            eye.setAttribute("title", "Reveal All");
+        }
+        // Toggles the titles of the emoji card
+        Object.values(document.getElementsByClassName("emoji-card-title")).forEach(title => {
+            title.classList.toggle("hide-card")
+        })
+    }
+}
