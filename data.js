37c0da3fc795604b82cdd6a31f67403fe99508d1
Code-Journal <hello@brittanywalker.io>
2019-08-30 10:05:14 -0400

    Update data.js

--- data.js
+++ data.js
@@ -386,7 +386,7 @@ const emojiItems = [
     },
     {
         title: "Lilo & Stitch",
-        emojiImgs: "🏄‍♀️👽👧🏽🌸",
+        emojiImgs: "🏄‍♀️👽👧🏽🌸"
         genres: ["animation", "comedy", "adventure"],
         type: "movie",
         year: 2002
