0b94dcf13b5fc1d15128d2f34ab60103bd5ddafc
colleen-chen <40923600+colleen-chen@users.noreply.github.com>
2019-08-29 20:17:55 -0400

    add "la la land"

--- data.js
+++ data.js
@@ -371,6 +371,13 @@ const emojiItems = [
         year: 2008
     },
     {
+	title: "La La Land",
+	emojiImgs: "👩👮💓",
+	genres: ["romance", "drama"],
+	type: "movie",
+	year: 2016
+    },
+    {
         title: "Legally Blonde",
         emojiImgs: "👩‍⚖️👠💘🏫🐶",
         genres: ["romance", "comedy"],
