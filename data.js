551acb92092ef9f4b77f69d8f1794f5f3ac4711e
Michael <mike.leibbrandt@gmail.com>
2019-09-26 15:27:08 +0200

    Fix Alf and Alien imdb  itemLinks

--- data.js
+++ data.js
@@ -36,7 +36,8 @@ const emojiItems = [
         emojiImgs: "🚀👽🍴🐈️",
         genres: ["comedy", "family", "sci-fi"],
         type: "tv",
-        year: 1986
+        year: 1986,
+        itemLink: "https://www.imdb.com/title/tt0090390"
     },
     {
         title: "Alien",
@@ -44,7 +45,7 @@ const emojiItems = [
         genres: ["suspense", "horror"],
         type: "movie",
         year: 1979,
-        itemLink: "https://www.imdb.com/title/tt0090390"
+        itemLink: "https://www.imdb.com/title/tt0078748/"
     },
     {
         title: "Avatar",
