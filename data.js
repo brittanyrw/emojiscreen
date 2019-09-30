cd3a791ca3128a26a18d659eaa1614b33466f655
Zester Quinn Albano <zesterquinn.albano@gmail.com>
2019-10-01 04:32:29 +0800

    Add Spirited Away (movie)

--- data.js
+++ data.js
@@ -758,6 +758,13 @@
         year: 2019
     },
     {
+        title: "Spirited Away",
+        emojiImgs: "👦👧👻🐉",
+        genres: ["animation", "adventure", "family"],
+        type: "movie",
+        year: 2001
+    },
+    {
         title: "SpongeBob SquarePants",
         emojiImgs: "🦀🍔🌊🍍🐿",
         genres: ["family", "animation"],
