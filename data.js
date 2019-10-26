00a46a927ff52e6e26051666305d4ef16162bf99
Jaye Hernandez <jaye@dealgrocer.com>
2019-10-26 23:27:20 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1354,6 +1354,14 @@ const emojiItems = [
     year: 2004
   },
   {
+    title: "Kinky Boots",
+    emojiImgs: "👢👢👑",
+    genres: ["musical", "drama", "comedy"],
+    type: "musical",
+    year: 2013,
+    itemLink: "http://www.playbill.com/production/kinky-boots-al-hirschfeld-theatre-vault-0000013982"
+  },
+  {
     title: "Kung Fu Panda",
     emojiImgs: "🐼🎍🇨🇳🥟🍚",
     genres: ["animation", "action", "adventure"],
@@ -1485,6 +1493,14 @@ const emojiItems = [
     type: "movie",
     year: 2010,
     itemLink: "https://www.imdb.com/title/tt0758752/"
+  },
+  {
+    title: "Mamma Mia",
+    emojiImgs: "👰💃🏻👑🌊",
+    genres: ["musical", "romance", "comedy"],
+    type: "movie",
+    year: 2008,
+    itemlink: "https://www.imdb.com/title/tt0795421/"
   },
   {
     title: "M*A*S*H",
@@ -2171,6 +2187,14 @@ const emojiItems = [
     type: "movie",
     year: 1992,
     itemLink: "https://www.imdb.com/title/tt0105417/?ref_=ttmi_tt"
+  },
+  {
+    title: "SIX: The Musical",
+    emojiImgs: "6️⃣👸🎤",
+    genres: ["musical", "drama", "history"],
+    type: "musical",
+    year: 2019,
+    itemLink: "http://www.playbill.com/production/six-the-musical-arts-theatre-2018-2019"
   },
   {
     title: "Sixth Sense",
