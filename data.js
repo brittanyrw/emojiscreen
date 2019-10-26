d6f5e287be74dea41858048f32cee04d9f4ded72
Cédric Nolin <ced.star.002@gmail.com>
2019-10-26 18:08:26 -0400

    Fix resident evil order based on the title alphabetically

--- data.js
+++ data.js
@@ -1987,6 +1987,14 @@ const emojiItems = [
     year: 1992
   },
   {
+    title: "Resident Evil",
+    emojiImgs: "🔫👩🦠🧟",
+    genres: [ "action", "horror", "sci-fi"],
+    type: "movie",
+    year: 2002,
+    itemLink: "https://www.imdb.com/title/tt0120804/"
+  },
+  {
     title: "The Revenant",
     emojiImgs: "🐻💥🤕❄️🥶",
     genres: ["action", "adventure", "biography"],
@@ -2618,12 +2626,5 @@ const emojiItems = [
     type: "movie",
     year: 2016
   },
-  {
-    title: "Resident Evil",
-    emojiImgs: "🔫👩🦠🧟",
-    genres: [ "action", "horror", "sci-fi"],
-    type: "movie",
-    year: 2002,
-    itemLink: "https://www.imdb.com/title/tt0120804/"
-  },
+  
 ];
