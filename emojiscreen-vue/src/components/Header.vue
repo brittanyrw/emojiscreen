9a3db684ca88dfc90dda40b8d7ac5bb418061744
Brittany Walker <breneewalker@gmail.com>
2020-01-27 16:20:20 -0500

    Add EmojiScreen HTML to Header component

--- emojiscreen-vue/src/components/Header.vue
+++ emojiscreen-vue/src/components/Header.vue
@@ -1,58 +1,32 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <header>
+        <h1>Emoji Screen</h1>
+        <div class="heading-content">
+            <div class="header">
+                <h2>A listing of movies, TV shows and musicals as emojis. 💖</h2>
+                <p>Click on the emojis to reveal the show or movie name!</p>
+                <div class="imdb-link">
+                    <i class="fas fa-film"></i>
+                    <a href="https://www.imdb.com/list/ls095903435/" target="_blank" class="imdb-link-item">
+                        View IMDB List
+                    </a>
+                </div>
+            </div>
+            <div class="contribute-callout">
+                <p>Want to add to EmojiScreen? Head to Github! ✨</p>
+                <a href="https://github.com/brittanyrw/emojiscreen" target="_blank">Contribute</a>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
 export default {
-  name: 'Header',
-  props: {
-    msg: String
-  }
+  name: 'Header'
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
